55dbb332fcf11bbf95d069c4e55214fcab97ec10
FidelineIngabire <fidelining09@gmail.com>
2024-04-27 16:28:16 +0300

    error handling in main file

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
-// const { notFound, errorHandler } = require("./middlewares/errorHandler");
+const bodyParser = require("body-parser");
 const dbConnect = require("./config/databaseConnect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
+
 dbConnect()
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// const cookieParser = require('cookie-parser');
-// const userRoute = require('./routes/userRoute')
-// const eventRoute = require('./routes/eventRoute')
-// const bookingRoute = require('./routes/bookingRoute')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const userRoute = require('./routes/userRoute')
+const eventsRoute = require('./routes/eventsRoute')
+const bookingRoute = require('./routes/bookingRoute')
 app.use('/api/user',userRoute)
 app.use('/api/event',eventsRoute)
-// app.use('/api/booking',bookingRoute)
-// app.use(notFound);
-// app.use(errorHandler);
-// app.use(cookieParser());
+app.use('/api/booking',bookingRoute)
+app.use(notFound);
+app.use(errorHandler);
+app.use(cookieParser());
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`);
   });
